bc87a9527d15db135f3a37c92c3446b485b20262
Felipe Girardi <felipegirardifg@gmail.com>
2023-01-14 19:15:18 +0100

    2.12

---
+++ part2/countries-data/src/App.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const CountryForm = (props) =>
+  <form>
+    <div>find countries
+      <input value={props.country} onChange={props.handleCountryChange} />
+    </div>
+  </form>
+
+const Description = ({ countriesToShow }) => {
+  console.log(countriesToShow)
+  const nCountries = countriesToShow.length
+  if (nCountries > 10) {
+    return <div>Too many matches, specify another filter</div>
+  } else if (nCountries > 1) {
+    return <div>{countriesToShow.map(c => <li key={c.name.common}>{c.name.common}</li>)}</div>
+  } else if (nCountries === 1) {
+    console.log("found 1 country")
+    return <CountryInfo countryData={countriesToShow[0]} />
+  } else if (nCountries === 0) {
+    return <div></div>
+  }
+}
+
+const CountryInfo = ({ countryData }) => 
+  <div>
+    <h2>{countryData.name.common}</h2>
+    <div>capital {countryData.capital}</div>
+    <div>capital {countryData.area}</div>
+    <h4>languages:</h4>
+    <ul>
+      {Object.values(countryData.languages).map(lang => <li key={lang}>{lang}</li>)}
+    </ul>
+    <img src={countryData.flags.png} alt="country flag" />
+  </div>
+
+const App = () => {
+  const [country, setCountry] = useState('')
+  const [countries, setCountries] = useState({})
+
+  useEffect(() => {
+    console.log('Effect')
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('Promise fulfilled')
+        setCountries(response.data)
+      })
+  }, [])
+
+  const handleCountryChange = (event) => {
+    const currentCountryValue = event.target.value
+    setCountry(currentCountryValue)
+  }
+
+  const countriesToShow = country
+    ? countries.filter(c => c.name.common.toLowerCase().includes(country.toLowerCase()))
+    : []
+
+  return (
+    <div>
+      <CountryForm country={country} handleCountryChange={handleCountryChange} />
+      <Description countriesToShow={countriesToShow} />
+    </div>
+  )
+}
+
+export default App;
