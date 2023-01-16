f8e4c9867771565cedaa1b8b0405e03dc4113541
Felipe Girardi <felipegirardifg@gmail.com>
2023-01-16 17:32:57 +0100

    2.14

--- part2/countries-data/src/App.js
+++ part2/countries-data/src/App.js
@@ -16,7 +16,6 @@ const Description = ({ countriesToShow }) => {
   } else if (nCountries > 1) {
     return <CountryList countriesToShow={countriesToShow} />
   } else if (nCountries === 1) {
-    console.log("found 1 country")
     return <CountryInfo countryData={countriesToShow[0]} />
   } else if (nCountries === 0) {
     return <div></div>
@@ -25,23 +24,35 @@ const Description = ({ countriesToShow }) => {
 
 const CountryItem = ({ country }) => {
   const [showCountryInfo, setShowCountryInfo] = useState(false)
+  const [weatherInfo, setWeatherInfo] = useState({})
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${country.capital[0]},${country.cca2}&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(response => {
+        setWeatherInfo(response.data)
+      })
+  }, [country.capital, country.cca2])
+
   return (
     <li key={country.name.common}>
       {country.name.common} <button onClick={() => setShowCountryInfo(!showCountryInfo)}>{showCountryInfo ? "hide" : "show"}</button>
       {showCountryInfo ? <CountryInfo countryData={country} /> : <div></div>}
+      {showCountryInfo ? <WeatherInfo weatherData={weatherInfo} /> : <div></div>}
+      <span>&nbsp;&nbsp;</span>
     </li>)
 }
 
 const CountryList = ({ countriesToShow }) => {
   return <div>{countriesToShow.map(c =>
-    <CountryItem country={c} />
+    <CountryItem key={c.name.common} country={c} />
   )}</div>
 }
 
 const CountryInfo = ({ countryData }) => 
   <div>
     <h2>{countryData.name.common}</h2>
-    <div>capital {countryData.capital}</div>
+    <div>capital {countryData.capital[0]}</div>
     <div>capital {countryData.area}</div>
     <h4>languages:</h4>
     <ul>
@@ -50,16 +61,24 @@ const CountryInfo = ({ countryData }) =>
     <img src={countryData.flags.png} alt="country flag" />
   </div>
 
+const WeatherInfo = ({ weatherData }) => {
+  console.log(weatherData)
+  return <div>
+    <h2>Weather in {weatherData.name}</h2>
+    <div>temperature {Math.round(weatherData.main.temp - 273.15)} Celsius</div>
+    <img src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`} alt="weather icon" />
+    <div>wind {weatherData.wind.speed} m/s</div>
+  </div>
+}
+
 const App = () => {
   const [country, setCountry] = useState('')
   const [countries, setCountries] = useState({})
 
   useEffect(() => {
-    console.log('Effect')
     axios
       .get('https://restcountries.com/v3.1/all')
       .then(response => {
-        console.log('Promise fulfilled')
         setCountries(response.data)
       })
   }, [])
@@ -76,6 +95,7 @@ const App = () => {
   return (
     <div>
       <CountryForm country={country} handleCountryChange={handleCountryChange} />
+      <span>&nbsp;&nbsp;</span>
       <Description countriesToShow={countriesToShow} />
     </div>
   )
